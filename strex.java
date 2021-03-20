452d57a129677ddf8ccaebf530b9eb6f54f6f4f7
Karthiga nandhan <40882132+Karthiganandhan@users.noreply.github.com>
2021-03-20 14:09:50 +0530

    Add files via upload

---
+++ strex.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+     String s=null;
+     String s1="";
+     String s2="karthiganandhan";
+     String s3="karthi";
+     String s4="Karthi";
+     String s5="surya prakash";
+     
+     
+    System.out.println("contains method"+s2.contains(s3));
+   System.out.println("contains method"+s2.contains(s4));
+    System.out.println("is empty meth"+s1.isEmpty());
+    
+  
+  System.out.println("Replace  meth:"+s5.replace("surya","chandra"));
+   
+   
+   System.out.println("is empty meth"+s.isEmpty());
+   
+   }
+   
+   
+ }
